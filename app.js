d36e3906fd040efed31a076fa47796f4f62ffb35
atikur-rabbi <msawon94@gmail.com>
2020-09-14 11:00:49 +0600

    Updated at 2020-09-14_1100 AM

--- app.js
+++ app.js
@@ -15,13 +15,4 @@ app.use('/music', serveIndex(__dirname + '/public/music'));
  app.use(express.static(__dirname + '/public', { maxAge: hourMs }));
 
 
- app.get('/audioFile.mp3', function(req, res) {
-    https.get('https://wish2.soundsip.com/box/para/files/Audio%20Stories/Bengali%20Audio%20Stories/Sunday%20Suspense/Roon-er%20Montro%20-%20MR%20James%20SS%2064ks.mp3', function(audioFile) {
-      res.set(_.extend(_.pick(audioFile.headers, 'accept-ranges', 'content-type', 'content-length'), { 'Access-Control-Allow-Origin': '*' }));
-      audioFile.pipe(res);
-    }).on('error', function(err) {
-      console.error(err);
-    }).end();
-  });
-
 module.exports = app
