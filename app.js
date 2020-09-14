b523afe55aa18cea37aa87dfca52738bae9b534f
atikur-rabbi <msawon94@gmail.com>
2020-09-14 09:55:05 +0600

    Updated at 2020-09-14_0955 AM

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
 
+var serveIndex = require('serve-index')
+
 app.get('/api', (req, res) => {
     res.send('Hello World!')
 })
 
 
 app.use(express.static(__dirname + '/public'));
+app.use('/music', serveIndex(__dirname + '/public/music'));
 
 module.exports = app
